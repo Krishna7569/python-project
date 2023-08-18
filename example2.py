bc99c177876efaad50d08b5f9afab7ac7c2e539e
Krishna Babu <141921262+Krishna7569@users.noreply.github.com>
2023-08-18 20:43:26 +0530

    Delete example2.py

--- example2.py
+++
@@ -1,9 +0,0 @@
-str=eval(input("Enter the line what you want:"))
-new_line=""
-f1=open("D:\\gopi\\file.txt","r")
-for i in f1:
-    if str:=str[new_line]:
-        a=str[::2].upper()
-        print(a)
-    else:
-        new_line+=line[i]
