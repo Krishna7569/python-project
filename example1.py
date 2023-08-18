808dd45cbad0c5f62f2954a94bc526b6c63e3133
Krishna Babu <141921262+Krishna7569@users.noreply.github.com>
2023-08-18 20:34:39 +0530

    Add files via upload

---
+++ example1.py
@@ -0,0 +1,14 @@
+str=int(input("Enter the line what you want:"))
+line_search=1
+f1=open("D:\\gopi\\file.txt","r")
+for x in f1:
+    if [line_search]:
+        a=str[::2]
+        print(x)
+        
+        break
+    else:
+        line_search=line_search+1
+l=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+for i in l:
+    print(i,x.count(i))
---
+++ example2.py
@@ -0,0 +1,9 @@
+str=eval(input("Enter the line what you want:"))
+new_line=""
+f1=open("D:\\gopi\\file.txt","r")
+for i in f1:
+    if str:=str[new_line]:
+        a=str[::2].upper()
+        print(a)
+    else:
+        new_line+=line[i]
